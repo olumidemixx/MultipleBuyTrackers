f1feb1df2c2579c79de926ae1e2caa10ad765f54
olumidemixx <OLUMIDEMIXX@GMAIL.COM>
2024-11-13 22:52:22 +0100

    Update multiplebuystrackooorr.py

--- multiplebuystrackooorr.py
+++ multiplebuystrackooorr.py
@@ -23,7 +23,7 @@ dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
 
 
-PORT = 8000 #int(os.getenv("PORT", 8443))
+PORT = int(os.getenv("PORT", 8000))
 
 # Telethon client configuration
 BOT_TOKEN = os.getenv("BOT_TOKEN")
